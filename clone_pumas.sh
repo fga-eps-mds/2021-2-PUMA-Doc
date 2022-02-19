524766ddb0d3a879e0470c4ac86acb652b343e2a
Mexazonic <brunnohenriq697@gmail.com>
2022-02-19 04:55:05 -0300

    Adição Script Download Repositorios

---
+++ clone_pumas.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+git clone https://github.com/fga-eps-mds/2021-2-PUMA-Doc
+echo "PUMA DOCS"
+git clone https://github.com/fga-eps-mds/2021-2-PUMA-Frontend
+echo "Download Front"
+git clone https://github.com/fga-eps-mds/2021-2-PUMA-ApiGateway
+echo "Download API-Gateway"
+git clone https://github.com/fga-eps-mds/2021-2-PUMA-UserService
+echo "Download User Service"
+git clone https://github.com/fga-eps-mds/2021-2-PUMA-ProjectService
+echo "Download Project Service"
+git clone https://github.com/fga-eps-mds/2021-2-PUMA-NotifyService
+echo "Download NotifyService"
+git clone https://github.com/fga-eps-mds/2021-2-PUMA-AlocateService
+echo "Download AlocateService"
